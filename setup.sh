e058e569f4577ea2334b7aeb13c088df0a1ccb7d
Robert Hawk <robert@the-hawk.us>
2016-12-19 00:58:11 +0000

    Fixed setup.sh to use absolute paths

--- setup.sh
+++ setup.sh
@@ -2,46 +2,37 @@
 
 # THUS Utils
 mkdir ~/bin
-cd ~/bin
-ln -s ~/lib/thus_utils/pom.sh pom
-ln -s ~/lib/thus_utils/gzless.sh gzless
-ln -s ~/lib/thus_utils/update.sh update
+ln -s ~/lib/thus_utils/pom.sh ~/bin/pom
+ln -s ~/lib/thus_utils/gzless.sh ~/bin/gzless
+ln -s ~/lib/thus_utils/update.sh ~/bin/update
 
 # bash_profile and bashrc
 mkdir ~/.config/
-cd ~/.config/
-git clone git@git.the-hawk.us:misc/cfg/termrc.git
-cd ~
-ln -s .config/termrc/bash_profile .bash_profile
-ln -s .config/termrc/bashrc .bashrc
+git clone git@git.the-hawk.us:misc/cfg/termrc.git ~/.config/termrc
+ln -s .config/termrc/bash_profile ~/.bash_profile
+ln -s .config/termrc/bashrc ~/.bashrc
 
 # vimrc
 mkdir ~/.vim
-cd ~/.vim
-git clone git@git.the-hawk.us:misc/cfg/vimrc.git
+git clone git@git.the-hawk.us:misc/cfg/vimrc.git ~/.vim/vimrc
 ln -s vimrc/autoload
 ln -s vimrc/doc
 ln -s vimrc/ftplugin
 ln -s vimrc/plugin
 ln -s vimrc/syntax
-cd ~
-ln -s .vim/vimrc/vimrc .vimrc
-ln -s .vim/vimrc/vimrc .ideavimrc
-ln -s .vim/vimrc/gvimrc .gvimrc
+ln -s .vim/vimrc/vimrc ~/.vimrc
+ln -s .vim/vimrc/vimrc ~/.ideavimrc
+ln -s .vim/vimrc/gvimrc ~/.gvimrc
 
 # taskrc
 mkdir ~/.task
-cd ~/.task
-git clone git@git.the-hawk.us:misc/cfg/taskrc.git
-cd ~
-ln -s .task/taskrc/taskrc .taskrc
+git clone git@git.the-hawk.us:misc/cfg/taskrc.git ~/.task/taskrc
+ln -s .task/taskrc/taskrc ~/.taskrc
 
 # i3
 mkdir -p ~/.config/i3
-cd ~/.config/i3
-git clone git@git.the-hawk.us:misc/cfg/i3rc.git
+git clone git@git.the-hawk.us:misc/cfg/i3rc.git ~/.config/i3/i3rc
 ln -s i3rc/config
 
 # timetrap
-cd ~
-ln -s Dropbox/databases/timetrap.db .timetrap.db
+ln -s Dropbox/databases/timetrap.db ~/.timetrap.db
