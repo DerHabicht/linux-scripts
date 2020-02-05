e22181fdd9a8b5a573117a7b150a1ed100067d69
Robert Herschel Hawk <robert@the-hawk.us>
2020-02-04 22:30:35 -0800

    Removed quotes, backslash, and redundant braces

--- randchar.py
+++ randchar.py
@@ -2,10 +2,11 @@ from random import choices
 
 
 def rand_nonalpha_chars():
-    chars = [chr(x) for x in range(0x21, 0x30)]
+    chars = [chr(0x21), chr(0x28), chr(0x5B), chr(0x7B), chr(0x7C), chr(0x7E)]
+    chars += [chr(x) for x in range(0x23, 0x27)]
+    chars += [chr(x) for x in range(0x2A, 0x30)]
     chars += [chr(x) for x in range(0x3A, 0x41)]
-    chars += [chr(x) for x in range(0x5B, 0x61)]
-    chars += [chr(x) for x in range(0x7A, 0x7E)]
+    chars += [chr(x) for x in range(0x5E, 0x60)]
 
     return choices(chars, k=2)
 
